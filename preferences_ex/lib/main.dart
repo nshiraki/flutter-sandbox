48d8d3d0e7a9b0954353a4960a22e6374bf238e4
Nobutaka Shiraki <shiraki.devi@gmail.com>
2022-08-08 12:11:49 +0900

    Widgetを追加

--- preferences_ex/lib/main.dart
+++ preferences_ex/lib/main.dart
@@ -29,13 +29,56 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (text) {
+                  // NOP
+                },
+              ),
+              // 保存ボタン
+              ElevatedButton(
+                  onPressed: () {
+                    // TODO: データを保存
+                  },
+                  child: const Text('Save')),
+              // 読み込みボタン
+              ElevatedButton(
+                  onPressed: () {
+                    // TODO: データを読み込み
+                  },
+                  child: const Text('Load')),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
