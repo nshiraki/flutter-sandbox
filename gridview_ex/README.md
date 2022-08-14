02b23a1a519f3e1292b011cdaf43d6d615fddb3f
Nobutaka Shiraki <shiraki.devi@gmail.com>
2022-08-14 16:46:05 +0900

    GridViewを利用した処理を追加

--- gridview_ex/README.md
+++ gridview_ex/README.md
@@ -1,16 +1,3 @@
 # gridview_ex
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+<img src="https://user-images.githubusercontent.com/9344579/184527329-a995fcbd-ff16-474b-ac67-e2a99f271e4d.png" width="360px">
--- gridview_ex/lib/main.dart
+++ gridview_ex/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+/// GridViewを利用した処理を作成する
+/// - GridViewの構築はGridView.builder()を使用している
 void main() {
   runApp(const MyApp());
 }
@@ -27,13 +29,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // ダミーのアイテムを生成
+  final List<String> _itemList = List.generate(1000, (index) {
+    return 'Item ${index + 1}';
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('GridViewEx'),
       ),
-      body: Container(),
+      body: Center(
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            // 子Widgetのアスペクト比(w/h)
+            childAspectRatio: 5 / 4,
+            // Widget間の左右のスペース
+            crossAxisSpacing: 4,
+            // Widget間の上下のスペース
+            mainAxisSpacing: 4,
+            // 1列で表示するアイテム数
+            crossAxisCount: 2,
+          ),
+          // アイテムの要素数
+          itemCount: _itemList.length,
+          // リストアイテムを生成
+          itemBuilder: (BuildContext context, int index) {
+            final item = _itemList[index];
+
+            return Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.grey,
+              child: Text(
+                item,
+                style: const TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
