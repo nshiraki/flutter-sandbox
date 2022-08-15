e73287ed4d5ba39b28e7457a6a45a4fc7ed42d61
Nobutaka Shiraki <shiraki.devi@gmail.com>
2022-08-15 21:09:28 +0900

    app_settingsを利用してアプリから設定画面を表示する処理を追加

--- app_settings_ex/README.md
+++ app_settings_ex/README.md
@@ -1,16 +1,3 @@
 # app_settings_ex
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+https://user-images.githubusercontent.com/9344579/184636290-e09e593c-58a6-4b3b-82e0-3ead5cf9d2ea.mp4
--- app_settings_ex/lib/main.dart
+++ app_settings_ex/lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
 
+/// app_settingsを利用してアプリから設定画面を表示する処理を作成する
+/// 利用するパッケージ
+/// - https://pub.dev/packages/app_settings
 void main() {
   runApp(const MyApp());
 }
@@ -24,11 +28,95 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<ItemData> items = createListItems();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('AppSettingsEx'),
       ),
-      body: Container(),
+      body: Center(
+        /// リストを生成
+        child: ListView.separated(
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            final ItemData item = items[index];
+
+            // リストアイテムを生成
+            return ListTile(
+              title: Text(item.displayText),
+              onTap: () {
+                item.clickAction.call();
+              },
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+        ),
+      ),
     );
   }
+
+  /// アイテムデータのリストを生成
+  List<ItemData> createListItems() => [
+        const ItemData(
+          displayText: 'アプリ設定',
+          clickAction: AppSettings.openAppSettings,
+        ),
+        const ItemData(
+          displayText: 'WIFI設定',
+          clickAction: AppSettings.openWIFISettings,
+        ),
+        const ItemData(
+          displayText: '現在地設定',
+          clickAction: AppSettings.openLocationSettings,
+        ),
+        const ItemData(
+          displayText: 'セキュリティ設定',
+          clickAction: AppSettings.openSecuritySettings,
+        ),
+        const ItemData(
+          displayText: 'Bluetooth設定',
+          clickAction: AppSettings.openBluetoothSettings,
+        ),
+        const ItemData(
+          displayText: 'データローミング設定',
+          clickAction: AppSettings.openDataRoamingSettings,
+        ),
+        const ItemData(
+          displayText: '日付設定',
+          clickAction: AppSettings.openDateSettings,
+        ),
+        const ItemData(
+          displayText: 'ディスプレイ設定',
+          clickAction: AppSettings.openDisplaySettings,
+        ),
+        const ItemData(
+          displayText: '通知設定',
+          clickAction: AppSettings.openNotificationSettings,
+        ),
+        const ItemData(
+          displayText: 'サウンド設定',
+          clickAction: AppSettings.openSoundSettings,
+        ),
+        const ItemData(
+          displayText: '内部ストレージ設定',
+          clickAction: AppSettings.openInternalStorageSettings,
+        ),
+        const ItemData(
+          displayText: 'バッテリー設定',
+          clickAction: AppSettings.openBatteryOptimizationSettings,
+        ),
+      ];
+}
+
+/// リストで扱うアイテムデータ
+@immutable
+class ItemData {
+  /// 表示テキスト
+  final String displayText;
+
+  /// クリック時のアクション
+  final Future<void> Function({bool asAnotherTask}) clickAction;
+
+  const ItemData({required this.displayText, required this.clickAction});
 }
