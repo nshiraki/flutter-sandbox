bfbb1e00107538c107b51d2db68956cb886c003b
Nobutaka Shiraki <shiraki.devi@gmail.com>
2022-08-18 03:16:00 +0900

    StreamBuilderを利用して、Streamの値に応じてWidgetを更新する処理追加

--- stream_builder_ex/README.md
+++ stream_builder_ex/README.md
@@ -1,16 +1,3 @@
 # stream_builder_ex
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+https://user-images.githubusercontent.com/9344579/185212629-fe31e547-5424-46f4-a1ac-325e4557e985.mp4
--- stream_builder_ex/lib/main.dart
+++ stream_builder_ex/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// StreamBuilderを利用して、Streamの値に応じてWidgetを更新する処理を作成する
 void main() {
   runApp(const MyApp());
 }
@@ -22,13 +23,40 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
+  /// StreamBuilderで監視するStreamの定義
+  Stream<int> _countStream() async* {
+    // 一秒おきに数値を返却
+    for (var i = 1; i <= 10; i++) {
+      await Future.delayed(const Duration(seconds: 1));
+      yield i;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final data = _countStream();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('StreamBuilderEx'),
       ),
-      body: Container(),
+      body: Center(
+        child: DefaultTextStyle(
+          style: Theme.of(context).textTheme.headline5!,
+
+          child: StreamBuilder(
+            stream: data,
+            builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+              if (snapshot.hasData) {
+                return Text('Count: ${snapshot.data}');
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
     );
   }
 }
