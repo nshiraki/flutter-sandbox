12ed1a9e3fc3bb77771b793311c7fd7a263b3657
Nobutaka Shiraki <shiraki.devi@gmail.com>
2022-08-18 11:56:27 +0900

    flutter_reactive_bleのパッケージを追加

--- reactive_ble_ex/lib/main.dart
+++ reactive_ble_ex/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,7 +28,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
