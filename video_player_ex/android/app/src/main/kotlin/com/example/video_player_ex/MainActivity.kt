bf35eb95229a76b489096f26f95eca061c588a65
Nobutaka Shiraki <shiraki.devi@gmail.com>
2022-09-18 01:11:08 +0900

    プロジェクトを新規追加

---
+++ video_player_ex/android/app/src/main/kotlin/com/example/video_player_ex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.video_player_ex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ video_player_ex/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+/// video_playerパッケージを利用して動画を再生する処理を作成する
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('VideoPlayerEx'),
+      ),
+      body: Container(),
+    );
+  }
+}
