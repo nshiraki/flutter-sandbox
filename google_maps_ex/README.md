a8df0ae977693b73654a929d7f023886b4485d95
Nobutaka Shiraki <shiraki.devi@gmail.com>
2022-10-10 06:15:49 +0900

    Googleマップを表示する処理を作成

--- google_maps_ex/README.md
+++ google_maps_ex/README.md
@@ -1,16 +1,8 @@
 # google_maps_ex
 
-A new Flutter project.
+## How to Build
+ビルドする場合は、Google Maps PlatformからGoogle Maps用のAPIキーを取得して、AndroidManifestファイルにAPIキーを設定してください
+- https://mapsplatform.google.com/
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Screenshot
+<img src="screenshot/img.png" width="360px">
--- google_maps_ex/lib/main.dart
+++ google_maps_ex/lib/main.dart
@@ -1,6 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 /// Googleマップを表示する処理を作成する
+/// 利用パッケージ
+/// - https://pub.dev/packages/google_maps_flutter
+///
+/// ビルドする場合は、Google Maps PlatformからGoogle Maps用のAPIキーを取得して、
+/// AndroidManifestファイルにAPIキーを設定してください
+/// - https://mapsplatform.google.com/
 void main() {
   runApp(const MyApp());
 }
@@ -28,13 +37,97 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // GoogleMapControllerを格納
+  GoogleMapController? _mapController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('GoogleMapsEx'),
       ),
-      body: Container(),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            // Googleマップを生成
+            Expanded(
+              child: GoogleMap(
+                mapType: MapType.normal,
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: _getInitialCameraPosition(),
+              ),
+            ),
+            // 表示位置（カメラ位置）を変更するボタンを生成
+            Wrap(
+              children: [
+                _buildButton(
+                    displayText: '新宿',
+                    latLng: const LatLng(35.693825, 139.703356)),
+                _buildButton(
+                    displayText: '渋谷',
+                    latLng: const LatLng(35.6580339, 139.7016358)),
+                _buildButton(
+                    displayText: '品川',
+                    latLng: const LatLng(35.628471, 139.73876)),
+                _buildButton(
+                    displayText: '東京',
+                    latLng: const LatLng(35.6809591, 139.7673068)),
+                _buildButton(
+                    displayText: '上野',
+                    latLng: const LatLng(35.713612, 139.77703)),
+                _buildButton(
+                    displayText: '池袋',
+                    latLng: const LatLng(35.729503, 139.7109)),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  /// GoogleMap生成時に呼ばれる
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      _mapController = controller;
+    });
+  }
+
+  /// カメラの初期表示位置を返却
+  CameraPosition _getInitialCameraPosition() {
+    return const CameraPosition(
+      // 初期表示位置を東京にする
+      target: LatLng(35.6809591, 139.7673068),
+      zoom: 17.0,
+    );
+  }
+
+  /// 表示位置（カメラ位置）を変更するボタンを生成する
+  Widget _buildButton({
+    required String displayText,
+    required LatLng latLng,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0),
+      child: ElevatedButton(
+          onPressed: () => {_updateCameraPosition(latLng: latLng)},
+          child: Text(displayText)),
+    );
+  }
+
+  /// 表示位置（カメラ位置）を変更する
+  void _updateCameraPosition({required LatLng latLng}) {
+    setState(() {
+      _mapController?.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: latLng,
+            zoom: 17.0,
+          ),
+        ),
+      );
+    });
   }
 }
