0957061d57dbafcf42580db980a4d8319691a4c7
Nobutaka Shiraki <shiraki.devi@gmail.com>
2022-10-17 02:32:14 +0900

    プロジェクトを新規作成

---
+++ hero_ex/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+/// Heroを利用した処理を作成する
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HeroEx'),
+      ),
+      body: Container(),
+    );
+  }
+}
