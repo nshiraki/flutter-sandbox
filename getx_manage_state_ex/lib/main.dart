2c3ba9031482c5b34e22f81b109fb6b117c295b7
Nobutaka Shiraki <shiraki.devi@gmail.com>
2022-11-07 02:40:55 +0900

    GetXを利用して状態管理を行う処理を追加

--- getx_manage_state_ex/lib/main.dart
+++ getx_manage_state_ex/lib/main.dart
@@ -13,31 +13,58 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+/// ViewModel
+class MyHomePageViewModel extends GetxController {
+  // 監視するカウント値
+  RxInt count = 0.obs;
 
+  /// カウント値を加算
+  void incrementCount() {
+    count.value++;
+  }
+
+  /// <!-- GetxControllerのライフサイクル処理（確認用でdumpするだけ）
+  /// ウィジェットがメモリに割り当てられた直後に呼び出される。
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  void onInit() {
+    print('onInit');
+    super.onInit();
+  }
+
+  /// onInit()の1フレーム後に呼び出される
+  @override
+  void onReady() {
+    print('onReady');
+    super.onReady();
+  }
+
+  /// onDeleteメソッドの前に呼び出される。controllerがメモリから削除される直前に実行される。
+  @override
+  void onClose() {
+    print('onClose');
+    super.onClose();
+  }
+
+  /// GetxControllerのライフサイクル処理（確認用でdumpするだけ）-->
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+/// View
+class MyHomePage extends StatelessWidget {
+  // Get.putを利用して、viewModelの依存オブジェクトを注入
+  // NOTE: 他クラスで依存を注入しておいて、Get.findで依存オブジェクトを取り出すこともできる
+  final _viewModel = Get.put(MyHomePageViewModel());
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -52,15 +79,18 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Obx(
+              // 監視している値を表示
+              () => Text(
+                '${_viewModel.count.value}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => _viewModel.incrementCount(),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
