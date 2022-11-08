55cf8fca6404057f6be44481359ec607b7d12623
Nobutaka Shiraki <shiraki.devi@gmail.com>
2022-11-09 02:45:28 +0900

    GetXを利用してルート管理を行う処理を追加

--- getx_manage_route_ex/README.md
+++ getx_manage_route_ex/README.md
@@ -1,16 +1,7 @@
 # getx_manage_route_ex
+GetXを利用してルート管理を行う処理を作成する
 
-A new Flutter project.
+## Screenshot
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+|---|---|
+|<img src="screenshot/img1.png" width="360px">|<img src="screenshot/img2.png" width="360px">|
--- getx_manage_route_ex/lib/main.dart
+++ getx_manage_route_ex/lib/main.dart
@@ -1,68 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 /// GetXを利用してルート管理を行う処理を作成する
-void main() {
-  runApp(const MyApp());
-}
+/// 利用パッケージ
+/// - https://pub.dev/packages/get
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => MainPage()),
+        GetPage(name: '/second', page: () => const SecondPage()),
+      ],
+      home: MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+/// MainPageViewModel
+class MainPageViewModel extends GetxController {
+  /// SecondPageから返却された結果を監視
+  final RxString result = '----'.obs;
 
-  final String title;
+  /// 結果をクリアするボタンが押下されたときにView側から呼ばれる処理
+  void onClearResultButton() => result.value = '----';
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  /// SecondPageから結果が返却されたときに呼ばれる
+  /// [data]SecondPageから返却された結果
+  void onSecondPageResult(String data) {
+    result.value = data;
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+/// MainPage
+class MainPage extends StatelessWidget {
+  MainPage({Key? key}) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final viewModel = Get.put(MainPageViewModel());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('GetX Manage Route Ex'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            // SecondPageから返却された結果を表示
+            Obx(() => Text(
+                  'SecondPageResult: ${viewModel.result.value}',
+                  style: const TextStyle(fontSize: 24),
+                )),
+            // 余白
+            const SizedBox(height: 16),
+            // SecondPageから返却された結果の表示をクリアする
+            ElevatedButton(
+              child: const Text('clear SecondPageResult'),
+              onPressed: () => viewModel.onClearResultButton(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            // 余白
+            const SizedBox(height: 16),
+            // SecondPageに遷移するボタン
+            ElevatedButton(
+              child: const Text('Move to Second'),
+              onPressed: () async {
+                // SecondPageに渡すデータを生成
+                final args = <String, dynamic>{
+                  "message": "this is second page"
+                };
+                // SecondPageに遷移する
+                final result = await Get.toNamed('/second', arguments: args);
+                // SecondPageから返却された結果を受け取る
+                viewModel.onSecondPageResult(result);
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+/// SecondPage
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // 遷移元から引数で渡されたデータを表示
+            Text(Get.arguments['message'],
+                style: const TextStyle(fontSize: 24)),
+            // 余白
+            const SizedBox(height: 16),
+            // 前の画面に戻るボタン
+            ElevatedButton(
+              // MainPageに戻る
+              child: const Text('Go back'),
+              // ボタンを押下すると「success」という結果を返却して、前の画面に戻る
+              onPressed: () => Get.back(result: 'success'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
